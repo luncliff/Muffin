6b097573628260212a9f8e652b404994bd76dc84
Park DongHa <luncliff@gmail.com>
2021-05-09 01:39:19 +0900

    azure: activate AVD

--- scripts/azure-pipelines.sh
+++ scripts/azure-pipelines.sh
@@ -3,13 +3,15 @@
 # References
 #   https://docs.microsoft.com/en-us/azure/devops/pipelines/ecosystems/android?view=azure-devops
 #
-echo ${ANDROID_HOME}
+echo ${ANDROID_HOME} 1>&2
+
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'cmake;3.18.1'
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'ndk;22.1.7171670'
-# echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86_64'
-# echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis;x86_64' --force
-# $ANDROID_HOME/emulator/emulator -list-avds
 
-# nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
-# $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
-# $ANDROID_HOME/platform-tools/adb devices
+echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86_64'
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis;x86_64' --force
+$ANDROID_HOME/emulator/emulator -list-avds 1>&2
+
+nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82' 1>&2
+$ANDROID_HOME/platform-tools/adb devices 1>&2
