c801a45e86a28459a8bc02ab1aa3ebcef0bac86f
luncliff <luncliff@gmail.com>
2025-04-17 19:10:19 +0900

    build: fix errors between library and application

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.library") version libs.versions.agp.get() apply false
+    id("com.android.application") version libs.versions.agp.get() apply false
     alias(libs.plugins.kotlin.android) apply false
 }
